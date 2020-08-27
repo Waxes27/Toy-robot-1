4513e023bee1546da1b3d494faf28c665aa0b2ce
ndumasi <ndumasi@student.wethinkcode.co.za>
2020-08-27 11:05:39 +0200

    complete

--- robot.py
+++ robot.py
@@ -1,14 +1,21 @@
-
-
-# TODO: Decompose into functions
-def move():
-    size = 10
+def move_square(size):
+    
     print("Moving in a square of size "+str(size))
     for i in range(4):
         degrees = 90
         print("* Move Forward "+str(size))
         print("* Turn Right "+str(degrees)+" degrees")
 
+
+def square_dancing(size):    
+    print("Square dancing - 3 squares of size 20")
+    for i in range(3):
+        length = 20
+        print("* Move Forward "+str(length))
+        move_square(size)
+
+
+def move_rectangle(length,width):
     length = 20
     width = 10
     print("Moving in a rectangle of "+str(length)+" by "+str(width))
@@ -19,6 +26,8 @@ def move():
         print("* Move Forward "+str(width))
         print("* Turn Right "+str(degrees)+" degrees")
 
+
+def move_circle():
     print("Moving in a circle")
     degrees = 1
     for i in range(360):
@@ -26,23 +35,24 @@ def move():
         print("* Move Forward "+str(length))
         print("* Turn Right "+str(degrees)+" degrees")
 
-    print("Square dancing - 3 squares of size 20")
-    for i in range(3):
-        length = 20
-        print("* Move Forward "+str(length))
-        print("Moving in a square of size 10")
-        for j in range(4):
-            print("* Move Forward " + str(size))
-            print("* Turn Right " + str(degrees) + " degrees")
-            degrees = 90
-
+    
+def crop_circles(length):
     print("Crop circles - 4 circles")
     for i in range(4):
         print("* Move Forward "+str(length))
-        print("Moving in a circle")
-        for k in range(360):
-            print("* Move Forward " + str(length))
-            print("* Turn Right " + str(degrees) + " degrees")
+        move_circle()
+
+
+
+
+# TODO: Decompose into functions
+def move():
+    move_square(10)
+
+    move_rectangle(20,10)
+    move_circle()
+    square_dancing(20)
+    crop_circles(20)
 
 
 def robot_start():
---
+++ robot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+python3 robot.py
+sleep 1.5
+clear
+done
